f257ca2ac537d7927dbfe720c6d9a8607e6d4e41
Leonardo Fernandes <fernandes.leo@live.com>
2022-07-05 11:26:38 -0300

    Initial Commit

---
+++ Shared/TeslaApp.swift
@@ -0,0 +1,17 @@
+//
+//  TeslaApp.swift
+//  Shared
+//
+//  Created by Cubix on 05/07/22.
+//
+
+import SwiftUI
+
+@main
+struct TeslaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
